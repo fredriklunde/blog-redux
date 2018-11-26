c91261fcf1d78a48240ec77b21d876b1012b92b7
Fredrik Lunde <fredrik.lunde3@gmail.com>
2018-11-26 16:27:03 +0100

    commenting out memoize

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+//import _ from 'lodash';
 
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
@@ -8,11 +8,19 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data })
 };
 
-export const fetchUser = (id) => dispatch => {
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data })
+}; 
+
+/*Memoized version
+
+/* export const fetchUser = (id) => dispatch => {
     _fetchUser(id, dispatch);
 }; 
 const _fetchUser = _.memoize(async (id, dispatch) => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({ type: 'FETCH_USER', payload: response.data })
-});
+}); */
